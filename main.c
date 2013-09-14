cf54e624381fde60c092cef1a42444195f9843ea
Benjamin <benjamin.villain@gmail.com>
2013-09-14 17:50:12 +0200

    Fix a bus error on freebsd Ignore errors from getch function (no key) Any key (except the one in use) will go back to the main menu

--- main.c
+++ main.c
@@ -419,10 +419,13 @@ static void process(screen_t *screen)
 {
     int c;
     const data_t *show_details;
-    
-    /* Force initial drawing */    
+
+    /* Force initial drawing */
     data_update(screen->datas);
     screen_update(screen, NULL);
+    show_details = NULL;
+
+    /* Initialize the var (might cause bus error in screen update) */
     show_details = NULL;
 
     while ((c = getch()) != 'Q' && c != 'q')
@@ -452,6 +455,8 @@ static void process(screen_t *screen)
             case 'h':
                 show_details = NULL;
 
+            /* no key striken (timeout), don't modify the screen state */
+            case ERR:
             default:
                 break;
         }
